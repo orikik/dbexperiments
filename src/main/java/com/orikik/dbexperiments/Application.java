e048f17d263cb63674a0bb1eed9442107a719d99
orikik <orikik@gmail.com>
2020-10-17 16:06:35 +0300

    add checkstyle, flyway and add db entities

---
+++ src/main/java/com/orikik/dbexperiments/Application.java
@@ -0,0 +1,13 @@
+package com.orikik.dbexperiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(com.orikik.dbexperiments.Application.class, args);
+    }
+
+}
